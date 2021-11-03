ee2ca338e97c17381950cae04496096cf4f0e244
leandrocunha526 <leandrocunha016@gmail.com>
2021-11-03 17:36:01 -0300

    Add product register form

--- angular-app/src/app/components/add-product/add-product.component.ts
+++ angular-app/src/app/components/add-product/add-product.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  product = {
+    name: "",
+    description: '',
+    price: '',
+    quantity: '',
+    aquisitionDate: ''
+  }
+  submitted = false;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
   }
 
+  createdProduct(): void {
+    const data = {
+      name: this.product.name,
+      description: this.product.description,
+      price: this.product.price,
+      quantity: this.product.quantity,
+      aquisitionDate: this.product.aquisitionDate
+    };
+    this.productService.create(data).subscribe(
+      response => {
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  newProduct(): void {
+    this.submitted = false,
+    this.product = {
+      name: '',
+      description: '',
+      price: '',
+      quantity: '',
+      aquisitionDate: ''
+    }
+  }
 }
