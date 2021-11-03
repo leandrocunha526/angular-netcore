0fb978f368c66a14b5a2e82f05a18e129397fe38
leandrocunha526 <leandrocunha016@gmail.com>
2021-11-03 14:34:03 -0300

    Add services

---
+++ angular-app/src/app/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+const baseURL = 'http://localhost:5000/api'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  rendAll(): Observable<any> {
+    return this.httpClient.get(`${baseURL}/list`);
+  }
+
+  read(id: any): Observable<any> {
+    return this.httpClient.get(`${baseURL}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.httpClient.post(`${baseURL}/register`, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.httpClient.put(`${baseURL}/edit/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.httpClient.delete(`${baseURL}/${id}`);
+  }
+}
