eb04cbd7d08ec24a591a9908bd92e6d7f738e3c9
leandrocunha526 <leandrocunha016@gmail.com>
2021-11-03 22:38:42 -0300

    Add edit product

--- angular-app/src/app/components/product-edit/product-edit.component.ts
+++ angular-app/src/app/components/product-edit/product-edit.component.ts
@@ -1,15 +1,51 @@
+import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  styleUrls: ['./product-edit.component.css'],
 })
 export class ProductEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  currentProduct = {
+    id: "",
+    name: '',
+    description: '',
+    price: '',
+    quantity: '',
+    aquisitionDate: ''
   }
 
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getProduct(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getProduct(id: any): void {
+    this.productService.read(id).subscribe(
+      (product) => {
+        this.currentProduct = product;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  updateProduct(id: any): void {
+    this.productService.update(id, this.currentProduct).subscribe(
+      (response) => {
+        this.router.navigate(['products']);
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
 }
